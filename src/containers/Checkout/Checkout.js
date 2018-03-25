9278fcb3fed65a186188aa512cdef8affefda698
Igor Kolga <igorkolga@hotmail.com>
2018-03-25 15:18:40 +0100

    Add redirect back to home page if reloaded on checkout and there are no ingredient data

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 
 import { connect } from 'react-redux';
 
@@ -16,15 +16,23 @@ class Checkout extends Component {
   }
 
   render () {
+    let summary = <Redirect to="/" />;
+    if (this.props.ings) {
+      summary = (
+        <div>
+          <CheckoutSummary
+            checkoutCancelled={this.checkoutCancelledHandler}
+            checkoutContinued={this.checkoutContinuedHandler}
+            ingredients={this.props.ings} />
+          <Route
+            path={this.props.match.path + '/contact-data'}
+            component={ContactData} />
+        </div>
+      );
+    }
     return (
       <div>
-        <CheckoutSummary
-          checkoutCancelled={this.checkoutCancelledHandler}
-          checkoutContinued={this.checkoutContinuedHandler}
-          ingredients={this.props.ings} />
-        <Route
-          path={this.props.match.path + '/contact-data'}
-          component={ContactData} />
+        {summary}
       </div>
     );
   }
